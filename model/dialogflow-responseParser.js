be12a8422ad0c9ccb1816cc87429d18f8bf7f6b8
Prashanth Moorthy <prashanth.moorthy@sap.com>
2018-10-04 16:06:11 +0530

    Adding func to detect intent type and route it to correct function

--- model/dialogflow-responseParser.js
+++ model/dialogflow-responseParser.js
@@ -8,16 +8,30 @@ var DialogFlowRespParser = /** @class */ (function () {
     function DialogFlowRespParser() {
     }
     DialogFlowRespParser.prototype.determineMatchedIntent = function (dialogFlowResp) {
+        var _this = this;
         return new Promise(function (resolve, reject) {
             var resp = parseJson(dialogFlowResp);
             var queryResult = resp.queryResult;
             console.log("allRequiredParamsPresent : " + queryResult.allRequiredParamsPresent);
             if (queryResult.allRequiredParamsPresent == true) {
                 console.log("We have all required parameters.... ");
-                resolve(queryResult.intent.displayName);
+                //resolve(queryResult.intent.displayName)
+                _this.fullfillIntentWithName(queryResult.intent.displayName, dialogFlowResp);
             }
             resolve("Some Error man");
         });
+    };
+    DialogFlowRespParser.prototype.fullfillIntentWithName = function (intentName, response) {
+        //Prashanth : Make this an enum here..       
+        if (intentName == "getBankName") {
+            return this.fulfillGetBankNameIntent(response);
+        }
+        if (intentName == "getBankName - getCityName") {
+            return this.fulfillGetCityIntent(response);
+        }
+        if (intentName == "getBankName - getCityName - getBranchName") {
+            return this.fulfillGetBankBranchNameIntent(response);
+        }
     };
     DialogFlowRespParser.prototype.fulfillGetCityIntent = function (dialogFlowResp) {
         return new Promise(function (resolve, reject) {
@@ -31,6 +45,17 @@ var DialogFlowRespParser = /** @class */ (function () {
         });
     };
     DialogFlowRespParser.prototype.fulfillGetBankNameIntent = function (dialogFlowResp) {
+        return new Promise(function (resolve, reject) {
+            var resp = parseJson(dialogFlowResp);
+            var queryResult = resp.queryResult.queryText;
+            console.log("Query Result Bank Name : " + queryResult);
+            bankColl.getAllBranchesForBankNameInCity("Dena Bank", "Bangalore")
+                .then(function (matchedBranches) {
+                resolve("We found many Branches.... ");
+            });
+        });
+    };
+    DialogFlowRespParser.prototype.fulfillGetBankBranchNameIntent = function (dialogFlowResp) {
         return new Promise(function (resolve, reject) {
             var resp = parseJson(dialogFlowResp);
             var queryResult = resp.queryResult.queryText;
--- model/dialogflow-responseParser.ts
+++ model/dialogflow-responseParser.ts
@@ -18,10 +18,26 @@ export class DialogFlowRespParser {
             console.log("allRequiredParamsPresent : " +  queryResult.allRequiredParamsPresent)
             if (queryResult.allRequiredParamsPresent == true){
                 console.log("We have all required parameters.... ")
-                resolve(queryResult.intent.displayName)
+                //resolve(queryResult.intent.displayName)
+                this.fullfillIntentWithName(queryResult.intent.displayName,dialogFlowResp)
             }
             resolve("Some Error man")
         })
+    }
+
+    fullfillIntentWithName(intentName : string, response : string) : Promise<string>{
+      //Prashanth : Make this an enum here..       
+        if (intentName == "getBankName"){
+            return this.fulfillGetBankNameIntent(response)
+        }
+        
+        if (intentName == "getBankName - getCityName"){
+            return this.fulfillGetCityIntent(response) 
+        }
+
+        if (intentName == "getBankName - getCityName - getBranchName"){
+            return this.fulfillGetBankBranchNameIntent(response) 
+        }
     }
 
     fulfillGetCityIntent(dialogFlowResp : string) : Promise<string> {
@@ -41,6 +57,23 @@ export class DialogFlowRespParser {
     }
 
     fulfillGetBankNameIntent(dialogFlowResp : string) : Promise<string> {
+
+        return new Promise((resolve : any, reject : any) => {
+            let resp = parseJson(dialogFlowResp)
+
+            let queryResult = resp.queryResult.queryText
+
+            console.log("Query Result Bank Name : " + queryResult)
+
+           bankColl.getAllBranchesForBankNameInCity("Dena Bank","Bangalore")
+                .then((matchedBranches : Array<BankBranchDetail>) => {
+                resolve("We found many Branches.... ") 
+           })
+        })
+    }
+
+
+    fulfillGetBankBranchNameIntent(dialogFlowResp : string) : Promise<string> {
 
         return new Promise((resolve : any, reject : any) => {
             let resp = parseJson(dialogFlowResp)
